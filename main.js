1f285f1f604d298ee873b9b3e30da37d2989b7e5
Dalila Bellorin <93064578+dalilabellorin@users.noreply.github.com>
2021-10-24 01:04:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'dalilabellorin',
+  linkedin: 'in/dalila-bellorin-362a30112',
+  facebook: 'dalila.bellorin.58',
+  instagram: 'bellorindaza10',
+  twitter: 'BellorinDalila'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
